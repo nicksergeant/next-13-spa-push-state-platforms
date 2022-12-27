fd3d50a148d94689790102cbe00181c5a57d5aa9
Nick Sergeant <nsergeant@hubspot.com>
2022-12-27 09:34:50 -0500

    Set up separate nav links per base path.

--- app/sites/foo/layout.tsx
+++ app/sites/foo/layout.tsx
@@ -5,12 +5,24 @@ export default function FooLayout({ children }: { children: React.ReactNode }) {
     <>
       <header>
         <nav>
+          Nav links to a base path of `/`:
           <ul>
             <li>
               <Link href="/">Foo Home</Link>
             </li>
             <li>
               <Link href="/about">Foo About</Link>
+            </li>
+          </ul>
+        </nav>
+        <nav>
+          Nav links to a base path of `/sites/foo`:
+          <ul>
+            <li>
+              <Link href="/sites/foo">Foo Home</Link>
+            </li>
+            <li>
+              <Link href="/sites/foo/about">Foo About</Link>
             </li>
           </ul>
         </nav>
