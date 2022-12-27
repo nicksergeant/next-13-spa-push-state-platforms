656fbaab3d69456071a41ce44a3602eda3057e00
Nick Sergeant <nsergeant@hubspot.com>
2022-12-27 08:48:52 -0500

    Initial commit.

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {children}
+      </body>
+    </html>
+  );
+}
